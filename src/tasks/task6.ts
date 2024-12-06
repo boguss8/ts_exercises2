4a6289c636ad22c51601d998191217c43089cf32
s25867 <s25867@pjwstk.edu.pl>
2024-12-06 11:31:31 +0100

    dziala!!!

--- src/tasks/task6.ts
+++ src/tasks/task6.ts
@@ -5,24 +5,28 @@
  */
 
 type AJHSF = {
-    zdane: boolean,
-    isPies: boolean,
-    firmaDoZamkniecia: boolean,
-    czySzefMaZnajomychWZarządzie: boolean,
-}
+  zdane: boolean;
+  isPies: boolean;
+  firmaDoZamkniecia: boolean;
+  czySzefMaZnajomychWZarządzie: boolean;
+};
 
 export const WynikKontroli = () => {
-    return new Promise((resolve: (v:AJHSF)=>void, reject) => {
-        const wynik = {
-            zdane: true,
-            isPies: true,
-            firmaDoZamkniecia: false,
-            czySzefMaZnajomychWZarządzie: true,
-        }
-        resolve(wynik);
-    });
-}
+  return new Promise((resolve: (v: AJHSF) => void, reject) => {
+    const wynik = {
+      zdane: true,
+      isPies: true,
+      firmaDoZamkniecia: false,
+      czySzefMaZnajomychWZarządzie: true,
+    };
+    resolve(wynik);
+  });
+};
 
 //# ---
 
-export type TypKontroliZUrzędu = any;
+export type TypKontroliZUrzędu = ReturnType<
+  typeof WynikKontroli
+> extends Promise<infer Result>
+  ? Result
+  : never;
